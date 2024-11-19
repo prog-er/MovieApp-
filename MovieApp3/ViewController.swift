1771ce2814c6d86b936ee15a2b323c1eddf5c684
Karibayev Olzhas <karibayevolzhas@gmail.com>
2024-11-19 11:05:17 +0500

    Initial Commit

---
+++ MovieApp3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp3
+//
+//  Created by Olzhas Karibayev on 19.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
